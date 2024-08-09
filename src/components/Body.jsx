303e36ffe0d6217d93dcbadc33c8e0fb073483c8
naveenpm96 <naveenpm96@gmail.com>
2024-08-09 12:17:55 +0530

    api bugs

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,64 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import Login from "./Login";
-// import Browse from "./Browse";
-// import { RouterProvider, createBrowserRouter } from "react-router-dom";
-// import { onAuthStateChanged } from "firebase/auth";
-// import { auth } from "../utils/Firebase";
-// import SignIn from "./SignIn";
-// import { useDispatch } from "react-redux";
-// import { addUser, removeUser } from "../utils/ReduxStore/userSlice";
-// import ProtectedRoutes from "./ProtectedRoutes";
-
-// const Body = () => {
-//   const dispatch = useDispatch();
-//   const [isAuthenticted, setIsAuthenticated] = useState(false);
-//   const appRouter = createBrowserRouter([
-//     {
-//       path: "/",
-//       element: <Login />,
-//     },
-//     {
-//       path: "/sign-in",
-//       element: <SignIn />,
-//     },
-//     {
-//       element: <ProtectedRoutes isAuthenticated={isAuthenticted} />,
-//       children: [
-//         {
-//           path: "/browse",
-//           element: <Browse />,
-//         },
-//       ],
-//     },
-
-//     {
-//       path: "*",
-//       element: <h1>404 - Page not found.</h1>,
-//     },
-//   ]);
-
-//   useEffect(() => {
-//     onAuthStateChanged(auth, (user) => {
-//       if (user) {
-//         setIsAuthenticated(true);
-//         const { email, displayName, uid } = user;
-//         dispatch(addUser({ email: email, displayName: displayName, uid: uid }));
-//       } else {
-//         setIsAuthenticated(false);
-//         dispatch(removeUser());
-//       }
-//     });
-//   }, []);
-
-//   return (
-//     <div>
-//       <RouterProvider router={appRouter} />
-//     </div>
-//   );
-// };
-
-// export default Body;
-
 import React, { useEffect, useState } from "react";
 import Login from "./Login";
 import Browse from "../components/BrowsePage/Browse";
@@ -116,7 +55,7 @@ const Body = () => {
         },
         {
           path: "/gpt-search",
-          element: <GptSearch/>,
+          element: <GptSearch />,
         },
       ],
     },
